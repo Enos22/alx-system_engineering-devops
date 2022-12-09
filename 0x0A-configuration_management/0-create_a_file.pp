6d1829d3c0ce9fc7a03c2a41b548d56e89b4f1e4
Enos22 <Enos22>
2022-12-09 05:16:37 -0800

    create and install package

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet create a file
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  }
